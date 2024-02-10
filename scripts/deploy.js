4e26c2d8ddacd51667af3b6c4d83f8023ff582bf
JaafarKrayem <jaafarkrayem@gmail.com>
2024-02-10 07:23:27 +0200

    Edit deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,21 +2,23 @@
 const hre = require("hardhat");
 
 const USDTAddress = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F"
+const multiSigWallet = "0x"
 
 async function main() {
-  const KPNToken = await hre.ethers.deployContract("KPNToken")
+  const KPNToken = await hre.ethers.deployContract("KPNToken"[multiSigWallet])
   await KPNToken.waitForDeployment()
 
-  const LockingContract = await hre.ethers.deployContract("LockTokens", [KPNToken.target, USDTAddress])
+  const LockingContract = await hre.ethers.deployContract("LockTokens", [KPNToken.target, USDTAddress, multiSigWallet])
   await LockingContract.waitForDeployment()
 
-  const ClaimingContract = await hre.ethers.deployContract("ClaimTokens", [KPNToken.target])
+  const ClaimingContract = await hre.ethers.deployContract("ClaimTokens", [KPNToken.target, multiSigWallet])
   await ClaimingContract.waitForDeployment()
 
   console.log(`
   KPN Deployed to ${KPNToken.target} \n
   Locking Contract Deployed to ${LockingContract.target} with ${USDTAddress} as the rewards token \n
   Claiming Mining Contract Deployed to ${ClaimingContract.target} \n
+  Owner Wallet as MultiSigWallet: ${multiSigWallet}\n
   Please Write these addresses down for future reference
   `);
 }
