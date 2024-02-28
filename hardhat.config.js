fdfed3e3e47b8f70f729348efc83960f0571adc6
JaafarKrayem <jaafarkrayem@gmail.com>
2024-02-28 20:44:16 +0200

    return to mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,23 +6,13 @@ require("dotenv").config()
 module.exports = {
   solidity: "0.8.20",
   networks:{
-    mumbai:{
-      url: process.env.TESTNET_ALCHEMY_URL,
-      accounts: [process.env.POLYGON_DEPLOYER_PRIVATE],
-    },
-    bsctest:{
-      url: process.env.TESTNET_ANKR_BNB,
-      accounts: [process.env.POLYGON_DEPLOYER_PRIVATE],
-    },
     polygon:{
       url: process.env.MAINNET_ALCHEMY_URL,
       accounts: [process.env.POLYGON_DEPLOYER_PRIVATE],
     },
   },
-  etherscan: {
-    apiKey: {
-      polygonMumbai: "VJSDX7ZS384QXHIXV273XYD797Q6GUV65U",
-    },
-  },
+  etherscan:{
+    apiKey: process.env.POLYGONSCAN_API
+  }
   
 };
